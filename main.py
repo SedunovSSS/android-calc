eacf11e99b13e76486e38b5d288ff44fee542c5f
SedunovSSS <sedunov308@gmail.com>
2023-03-02 15:24:23 +0500

    calc

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ class Calc(App):
     def build(self):
         self.b_l = BoxLayout(orientation='vertical')
         self.layout = GridLayout(cols=4, row_force_default=False, row_default_height=20)
-        self.calc = TextInput(text='', multiline=True, font_size=80)
+        self.calc = TextInput(text='0', multiline=True, font_size=80, background_color="black", foreground_color="white")
         self.calc.size_hint = (1, 0.25)
         self.b_l.add_widget(self.calc)
-        self.calc.text = "0"
         self.btns = ['1', '2', '3', 'DEL',
                      '4', '5', '6', 'AC',
                      '7', '8', '9', '0',
                      '+', '-', '*', '/',
-                     '(', ')', '.', '='
-                     ]
+                     '(', ')', '.', '=']
         for i in self.btns:
             btn = Button(text=i, size_hint=(1, 0.5), font_size=60)
             if i == "AC":
-                btn.bind(on_press=self.clear)
+                btn.background_color = "red"
+                btn.bind(on_release=self.clear)
             elif i == "DEL":
-                btn.bind(on_press=self.delete)
+                btn.background_color = "red"
+                btn.bind(on_release=self.delete)
             elif i == "=":
-                btn.bind(on_press=self.calculate)
+                btn.background_color = "red"
+                btn.bind(on_release=self.calculate)
             else:
-                btn.bind(on_press=self.txt_insert)
+                if i.isdigit():
+                    btn.background_color = "blue"
+                else:
+                    btn.background_color = "lime"
+                btn.bind(on_release=self.txt_insert)
             self.layout.add_widget(btn)
 
         self.b_l.add_widget(self.layout)
